8f886868a5f870194e76f1726c415d5c648a65d8
Corentin Remaud <corentin@billjobs.fr>
2017-10-03 11:03:15 +0200

    Initial commit yo !

---
+++ README.md
@@ -0,0 +1,14 @@
+# python-allrecipes
+##### v0.1.6
+
+Python API to search &amp; get recipes from the 'allrecipes.com' website (web crawler, unofficial)  
+Useful, efficient and super simple to use.  
+
+### Installation :
+`pip install python-latin-expr-fr`  
+
+
+### Requirements :
+`python >= 3.4`  
+
+###### Support / Contact : remaudcorentin.dev@gmail.com
---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+	name='python-latin-expr-fr',
+	version='0.1.2',
+	description='Random Latin expression with French translations to upgrade your brain !',
+	packages=['latinexpr'],
+	url='https://github.com/remaudcorentin-dev/python-latin-expr-fr',
+	author='Corentin Remaud',
+	author_email='remaudcorentin.dev@gmail.com',
+	license='MIT',
+	zip_safe=False,
+	)
